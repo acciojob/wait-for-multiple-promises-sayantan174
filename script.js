b89bb7a80e29955f1ec7de378294f98921cff595
Sayantan Mandal <sayantanmandal174@gmail.com>
2023-05-21 17:52:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-//your JS code here. If required.
+let prom1 = new Promise((resolve) => {
+  let time = Math.floor(Math.random() * 10);
+  setTimeout(() => {
+    resolve(time);
+  }, time);
+});
+let prom2 = new Promise((resolve) => {
+  let time = Math.floor(Math.random() * 10);
+  setTimeout(() => {
+    resolve(time);
+  }, time);
+});
+let prom3 = new Promise((resolve) => {
+  let time = Math.floor(Math.random() * 10);
+  setTimeout(() => {
+    resolve(time);
+  }, time);
+});
+const output = document.getElementById("output");
+console.log(Promise.all([prom1, prom2, prom3]));
+console.log(output);
+const promise = Promise.all([prom1, prom2, prom3]);
+promise.then((value) => {
+  output.innerHTML = `<tr>
+  <td>Promise 1</td>
+  <td>${value[0]}</td>
+</tr><tr>
+<td>Promise 2</td>
+<td>${value[1]}</td>
+</tr><tr>
+<td>Promise 3</td>
+<td>${value[2]}</td>
+</tr>`;
+});
