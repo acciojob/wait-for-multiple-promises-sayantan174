b31f2e5b5c4f9d74f4696f25ce12704fecf67e70
Sayantan Mandal <sayantanmandal174@gmail.com>
2023-05-21 18:05:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,38 @@
 let prom1 = new Promise((resolve) => {
-  let time = Math.floor(Math.random() * 10);
+  let time = Math.floor((Math.random()*3+1) * 1000);
   setTimeout(() => {
     resolve(time);
   }, time);
 });
 let prom2 = new Promise((resolve) => {
-  let time = Math.floor(Math.random() * 10);
+  let time = Math.floor((Math.random()*3+1) * 1000);
   setTimeout(() => {
     resolve(time);
   }, time);
 });
 let prom3 = new Promise((resolve) => {
-  let time = Math.floor(Math.random() * 10);
+  let time = Math.floor((Math.random()*3+1) * 1000);
   setTimeout(() => {
     resolve(time);
   }, time);
 });
 const output = document.getElementById("output");
+output.innerHTML = ` <tr>
+<td>Loading...</td>
+<td>Loading...</td>
+</tr>`;
 console.log(Promise.all([prom1, prom2, prom3]));
 console.log(output);
 const promise = Promise.all([prom1, prom2, prom3]);
 promise.then((value) => {
   output.innerHTML = `<tr>
   <td>Promise 1</td>
-  <td>${value[0]}</td>
+  <td>${Math.floor(value[0]/1000)}</td>
 </tr><tr>
 <td>Promise 2</td>
-<td>${value[1]}</td>
+<td>${Math.floor(value[1]/1000)}</td>
 </tr><tr>
 <td>Promise 3</td>
-<td>${value[2]}</td>
+<td>${Math.floor(value[2]/1000)}</td>
 </tr>`;
 });
